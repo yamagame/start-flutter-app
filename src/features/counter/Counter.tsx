91e1c996f8202a2ac872d854362b3588162bc061
yamagame <yamagame@mac.com>
2022-01-02 16:51:22 +0900

    v3

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -56,6 +56,7 @@ export function Counter() {
     };
     const NFCScan = (message: any) => {
       console.log(`nfc`, JSON.stringify(message));
+      console.log(message.payload.tag);
     };
     applicationEvent.on("ble", BLEScan);
     applicationEvent.on("nfc", NFCScan);
@@ -92,7 +93,7 @@ export function Counter() {
   return (
     <div>
       <div className={styles.row}>
-        V2
+        V3
         <button
           className={styles.button}
           aria-label="Decrement value"
